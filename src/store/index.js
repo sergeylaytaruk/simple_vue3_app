c6d40d974b4567b4f3fc93d7303cce9a327826e8
sergey laytaruk <sergey.laytaruk@gmail.com>
2023-01-08 18:53:27 +0200

    2023-01-08

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,3 @@
-/*import Vue from 'vue'
-import Vuex from 'vuex'
-import post from './modules/post'
-*/
-/*Vue.use(Vuex);
-
-export default new Vuex.Store({
-    modules: {
-        post
-    }
-});*/
 import { createStore } from "vuex";
 import post from './modules/post';
 export default createStore({
